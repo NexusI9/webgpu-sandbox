e8cced5c5e53014b1c41d0c57993b923dc5fff68
NexusI9 <nassim.elkhantour@gmail.com>
2025-06-23 22:57:51 +0800

    set base raycast

--- runtime/raycast/core.h
+++ runtime/raycast/core.h
@@ -1,7 +1,27 @@
 #ifndef _RAYCAST_CORE_H_
 #define _RAYCAST_CORE_H_
 
+#include "../camera/camera.h"
+#include "../scene/scene.h"
+#include <cglm/cglm.h>
 
+#define RAYCAST_DISTANCE_INF 1000.0f
 
+typedef struct {
+  vec3 origin;
+  vec3 direction;
+  float distance;
+  bool hit;
+} Raycast;
+
+typedef struct {
+  vec3 origin;
+  vec3 direction;
+  float distance;
+} RaycastCreateDescriptor;
+
+void raycast_create(const RaycastCreateDescriptor *);
+void raycast_from_camera(Raycast *, const Camera *);
+void raycast_cast_mesh(Raycast *, MeshRefList *);
 
 #endif
