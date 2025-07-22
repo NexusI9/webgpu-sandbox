6daf368320a8d17734d016d2ab5aed8588d04b5e
NexusI9 <nassim.elkhantour@gmail.com>
2025-07-22 11:33:56 +0800

    improve verbose print

--- utils/system.h
+++ utils/system.h
@@ -6,14 +6,45 @@
 
 #ifdef VERBOSE
 #define VERBOSE_PRINT(...)                                                     \
-  printf("[%s:%d] ", __FILE__, __LINE__);                                      \
-  printf(__VA_ARGS__)
+  printf("ℹ️ ");                                                                \
+  printf(__VA_ARGS__);                                                         \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
 #define VERBOSE_ERROR(...)                                                     \
-  fprintf(stderr, "[%s:%d] ", __FILE__, __LINE__);                             \
-  fprintf(stderr, __VA_ARGS__)
+  fprintf(stderr, "❌ ");                                                      \
+  fprintf(stderr, __VA_ARGS__);                                                \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
 #define VERBOSE_WARNING(...)                                                   \
-  printf("[%s:%d] ", __FILE__, __LINE__);                                      \
-  printf(__VA_ARGS__)
+  printf("⚠️ ");                                                                \
+  printf(__VA_ARGS__);                                                         \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
+#define VERBOSE_DEBUG(...)                                                     \
+  printf("🐞 ");                                                               \
+  printf(__VA_ARGS__);                                                         \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
+#define VERBOSE_SHADER_CREATE(...)                                             \
+  printf("🎨 Creating shader: ");                                              \
+  printf(__VA_ARGS__);                                                         \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
+#define VERBOSE_MESH_BUILD(...)                                                \
+  printf("🧱 Building mesh: ");                                                \
+  printf(__VA_ARGS__);                                                         \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
+#define VERBOSE_MESH_CREATE(...)                                               \
+  printf("✨ Creating mesh: ");                                                \
+  printf(__VA_ARGS__);                                                         \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
+#define VERBOSE_IMPORT(...)                                                    \
+  printf("📦 Importing: ");                                                    \
+  printf(__VA_ARGS__);                                                         \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
+#define VERBOSE_SUCCESS(...)                                                   \
+  printf("☑️ ");                                                                \
+  printf(__VA_ARGS__);                                                         \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
+#define VERBOSE_PROCESS(...)                                                   \
+  printf("⚙️ ");                                                                \
+  printf(__VA_ARGS__);                                                         \
+  printf(" (%s:%d)\n", __FILE__, __LINE__)
 #define VERBOSE_HEADER(...)                                                    \
   printf("===== ");                                                            \
   printf(__VA_ARGS__);                                                         \
@@ -22,6 +53,13 @@
 #define VERBOSE_PRINT(...) // no-op
 #define VERBOSE_ERROR(...)
 #define VERBOSE_WARNING(...)
+#define VERBOSE_DEBUG(...)
+#define VERBOSE_SHADER_CREATE(...)
+#define VERBOSE_MESH_BUILD(...)
+#define VERBOSE_MESH_CREATE(...)
+#define VERBOSE_IMPORT(...)
+#define VERBOSE_SUCCESS(...)
+#define VERBOSE_PROCESS(...)
 #define VERBOSE_HEADER(...)
 #endif
 
