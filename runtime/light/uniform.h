b03878075f52432f03fe4e805cf04bba2dc61432
NexusI9 <nassim.elkhantour@gmail.com>
2025-09-11 23:25:48 +0800

    improve reflection normal to TBN

--- runtime/light/uniform.h
+++ runtime/light/uniform.h
@@ -11,7 +11,7 @@ typedef struct {
   float cutoff;
   vec3 color;
   float intensity;
-  mat4 views[LIGHT_POINT_VIEWS]; // TODO: move the views into projection SSBO
+  mat4 views[LIGHT_POINT_VIEWS];
   float inner_cutoff;
   float near;
   float far;
